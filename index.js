93cb387cb3f9ac51a09fa505ea0ed27d303d2c01
ceboulba <antoine@archipicture.fr>
2019-08-10 11:17:24 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // Import stylesheets
 import './style.css';
 
+const list = document.querySelector('.todos')
+const inputTodo = document.querySelector('.add') 
+
+const addTodo = (todo) => {
+const htmlTemplate = `
+  <li><span>${todo}</span> <i class="fas fa-trash"></i></li>
+`
+list.innerHTML += htmlTemplate
+}
+
+inputTodo.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const toDo = e.target.value.trim()
+  addTodo(toDo)
+  console.log(e.target.value)
+})
